7ab05c02a3641f791a6f83e0240106f7f656088d
luizfagundes <luiz_fagundess@hotmail.com>
2015-02-25 13:44:29 -0300

    2 Maprequests adicionados

--- Teste.java
+++ Teste.java
@@ -9,5 +9,14 @@ public class TesteController {
 	public String sayHello(){
 		return "Hello World!"
 	}
+        @RequestMapping(value="/alunos/{ra}",method=RequestMethod.GET)
+        public Aluno getAluno(@PathVariable Integer ra){
+                Aluno novo = new Aluno("Aluno Jr. "+ System.currentTimeMillis(), ra);
+                return novo;	
+	}
+	@RequestMapping(value="/alunos",method=RequestMethod.POST)
+        public String criarAluno(@RequestBody(required=true) Aluno novo) {
+                return "Aluno de nome [" + novo.getNome() + "] recebido com imenso sucesso!";
+	}
 	
 }
